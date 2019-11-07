c9805b7004971d6c1bf886d30f5bccbe706a2480
David ståhl <david.n.stahl@gmail.com>
2019-11-07 13:33:22 +0100

    Add project files.

---
+++ console.EntityFramwork/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace console.EntityFramwork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (NORTHWINDEntities db = new NORTHWINDEntities())
+            {
+                var cust = "P";
+                var customers = db.Customers.Include("Orders").Where(r => r.CompanyName.StartsWith(cust) && r.Orders.Any(order => order.OrderID > 10100)).ToList();
+                foreach (var item in customers)
+                {
+                    Console.WriteLine(item.CompanyName.ToString());
+                }                
+            }
+            Console.ReadLine();
+        }
+    }
+}
